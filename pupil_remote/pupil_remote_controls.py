ad17ebeae30e22e3893ba0e017ff423db89527d0
Will Patera <willpatera@gmail.com>
2016-05-04 17:33:59 +0700

    adding `pupil remote` control example

---
+++ pupil_remote/pupil_remote_controls.py
@@ -0,0 +1,32 @@
+"""
+This example demonstrates how to send simple messages to the Pupil Remote plugin
+	'R' start recording with auto generated session name
+	'R rec_name' start recording and name new session name: rec_name
+	'r' stop recording
+	'C' start currently selected calibration
+	'c' stop currently selected calibration
+	'T 1234.56' Timesync: make timestamps count form 1234.56 from now on.
+"""
+import zmq
+from time import sleep, time
+
+context =  zmq.Context()
+socket = context.socket(zmq.REQ)
+
+# set your IP here
+socket.connect('tcp://10.1.12.162:50020')
+t = time()
+socket.send('T 0.0') #set timebase to 0.0
+
+print socket.recv()
+print 'Round trip command delay:', time()-t
+print 'If you need continuous syncing and less latency look at pupil_sync.'
+
+sleep(1)
+socket.send('R')
+print socket.recv()
+
+sleep(5)
+socket.send('r')
+
+print socket.recv()
