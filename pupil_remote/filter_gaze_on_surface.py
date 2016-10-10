9244dfd9d610f05ea8aef7e571019abc1214d7d8
Will Patera <willpatera@gmail.com>
2016-10-10 11:49:38 +0700

    update for v0.8.5

--- pupil_remote/filter_gaze_on_surface.py
+++ pupil_remote/filter_gaze_on_surface.py
@@ -10,7 +10,7 @@ context = zmq.Context()
 
 #open a req port to talk to pupil
 addr = '127.0.0.1' # remote ip or localhost
-req_port = "50020" # same as in the pupil remote gui
+req_port = "65033" # same as in the pupil remote gui
 req = context.socket(zmq.REQ)
 req.connect("tcp://%s:%s" %(addr,req_port))
 # ask for the sub port
@@ -19,15 +19,23 @@ sub_port = req.recv()
 # open a sub port to listen to pupil
 sub = context.socket(zmq.SUB)
 sub.connect("tcp://%s:%s" %(addr,sub_port))
-sub.setsockopt(zmq.SUBSCRIBE, 'gaze')
+sub.setsockopt(zmq.SUBSCRIBE, 'surface')
+
+# specify the name of the surface you want to use
 surface_name = 'unnamed'
 
 while True:
     topic,msg =  sub.recv_multipart()
-    gaze_position = loads(msg)
+    surfaces = loads(msg)
+    filtered_surface = {k: v for k, v in surfaces.iteritems() if surfaces['name'] == surface_name}
     try:
-        gp_x, gp_y = gaze_position["realtime gaze on "+surface_name]
-        if (0<= gp_x <=1 and 0<= gp_y <=1):
-            print "gaze on surface: %s\t - normalized coords: %s, %s" %(surface_name, gp_x, gp_y)
-    except KeyError:
+        # note that we may have more than one gaze position data point (this is expected behavior)
+        gaze_positions = filtered_surface['gaze_on_srf']
+        for gaze_pos in gaze_positions:
+            norm_gp_x, norm_gp_y = gaze_pos['norm_pos']
+
+            # only print normalized gaze positions within the surface bounds
+            if (0<= norm_gp_x <=1 and 0<= norm_gp_y <=1):
+                print "gaze on surface: %s\t, normalized coordinates: %s,%s" %(surface_name, norm_gp_x,norm_gp_y)
+    except:
         pass
