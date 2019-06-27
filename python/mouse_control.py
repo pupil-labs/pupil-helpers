6ed5ae7d4cdc3abe69bb7a277388a5f28f992e15
Pablo Prietz <pablo@prietz.org>
2019-06-27 17:25:11 -0600

    Update mouse_control.py

--- python/mouse_control.py
+++ python/mouse_control.py
@@ -4,6 +4,9 @@ Please note that marker tracking must be enabled, and in this example we have na
 You can name the surface what you like in Pupil capture and then write the name of the surface you'd like to use on line 17.
 """
 
+# specify the name of the surface you want to use
+surface_name = "screen"
+
 ## install dependencies
 # pip3 install zmq msgpack pyuserinput
 
@@ -14,37 +17,52 @@ from platform import system
 
 if system() != "Darwin":
     from pymouse import PyMouse
+
     m = PyMouse()
     m.move(0, 0)  # hack to init PyMouse -- still needed
 
 
-def move_mouse(x, y):
+def move_mouse(x, y, click=False):
     if system() == "Darwin":
-        sp.Popen(["./mac_os_helpers/mouse", "-x", str(x), "-y", str(y), "-click", str(click)])
+        sp.Popen(
+            [
+                "./mac_os_helpers/mouse",
+                "-x",
+                str(x),
+                "-y",
+                str(y),
+                "-click",
+                str(int(click)),
+            ]
+        )
     else:
         m.move(x, y)
 
+
 def get_screen_size():
     if system() == "Darwin":
-        screen_size = sp.check_output(["./mac_os_helpers/get_screen_size"]).decode().split(",")
+        screen_size = (
+            sp.check_output(["./mac_os_helpers/get_screen_size"]).decode().split(",")
+        )
         return float(screen_size[0]), float(screen_size[1])
     else:
         return m.screen_size()
 
+
 context = zmq.Context()
 # open a req port to talk to pupil
-addr = '127.0.0.1'  # remote ip or localhost
+addr = "127.0.0.1"  # remote ip or localhost
 req_port = "50020"  # same as in the pupil remote gui
 req = context.socket(zmq.REQ)
 req.connect("tcp://{}:{}".format(addr, req_port))
 # ask for the sub port
-req.send_string('SUB_PORT')
+req.send_string("SUB_PORT")
 sub_port = req.recv_string()
 
 # open a sub port to listen to pupil
 sub = context.socket(zmq.SUB)
 sub.connect("tcp://{}:{}".format(addr, sub_port))
-sub.setsockopt_string(zmq.SUBSCRIBE, 'surface')
+sub.setsockopt_string(zmq.SUBSCRIBE, f"surfaces.{surface_name}")
 
 smooth_x, smooth_y = 0.5, 0.5
 
@@ -52,13 +70,11 @@ smooth_x, smooth_y = 0.5, 0.5
 x_dim, y_dim = get_screen_size()
 print("x_dim: {}, y_dim: {}".format(x_dim, y_dim))
 
-# specify the name of the surface you want to use
-surface_name = "screen"
 while True:
     topic, msg = sub.recv_multipart()
-    gaze_position = loads(msg, encoding='utf-8')
-    if gaze_position['name'] == surface_name:
-        gaze_on_screen = gaze_position['gaze_on_srf']
+    gaze_position = loads(msg, encoding="utf-8")
+    if gaze_position["name"] == surface_name:
+        gaze_on_screen = gaze_position["gaze_on_surfaces"]
         if len(gaze_on_screen) > 0:
 
             # there may be multiple gaze positions per frame, so you could average them
@@ -66,20 +82,20 @@ while True:
             # raw_y = sum([i['norm_pos'][1] for i in gaze_on_screen])/len(gaze_on_screen)
 
             # or just use the most recent gaze position on the surface
-            raw_x, raw_y = gaze_on_screen[-1]['norm_pos']
+            raw_x, raw_y = gaze_on_screen[-1]["norm_pos"]
 
             # smoothing out the gaze so the mouse has smoother movement
-            smooth_x += 0.35 * (raw_x-smooth_x)
-            smooth_y += 0.35 * (raw_y-smooth_y)
+            smooth_x += 0.35 * (raw_x - smooth_x)
+            smooth_y += 0.35 * (raw_y - smooth_y)
             x = smooth_x
             y = smooth_y
 
-            y = 1-y  # inverting y so it shows up correctly on screen
+            y = 1 - y  # inverting y so it shows up correctly on screen
             x *= int(x_dim)
             y *= int(y_dim)
             # PyMouse or MacOS bugfix - can not go to extreme corners because of hot corners?
-            x = min(x_dim-10, max(10, x))
-            y = min(y_dim-10, max(10, y))
+            x = min(x_dim - 10, max(10, x))
+            y = min(y_dim - 10, max(10, y))
 
             # print "%s,%s\n" %(x,y)
             move_mouse(int(x), int(y))
