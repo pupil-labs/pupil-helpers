b0c7054e03418bb8e03f987d18ed42431cc1064e
Pablo Prietz <pablo@prietz.org>
2021-04-26 11:06:32 +0200

    Applied @n-m-t 's suggestions

--- python/simple_realtime_time_sync.py
+++ python/simple_realtime_time_sync.py
@@ -1,50 +1,59 @@
 """Simple Time-Sync
 
 ## Introduction
+Eye tracking is often performed concurrently with external recording devices (e.g.
+physiological sensors, motion capture) and/or software (e.g. for stimuli presentation,
+experiment control). In order to correlate the eye tracking data with those of the
+external devices/software, temporal alignment is of great importance.
 
-Eye-tracking data is often analysed in the context of externally controlled stimuli
-or other physiological sensors, e.g. EEG. In order to correlate the recorded data, its
-temporal alignment is of importance. By default, most sensors or experiment softwares
-come with their own clock to measure time consistently. These clocks rarely share a
-common starting point which would allow for automatic time alignment. In return, these
-clocks can guarantee to be monotonicly increasing and very accurate when it comes to
-measuring time differences. Pupil Core software is no exception in this regard. In the
-example below, we show how one can calculate the offset between a custom "local" clock
-and the clock used in Pupil Core software in real time.
+By default, most devices/software come with their own clock to measure time
+consistently. These clocks rarely share a common starting point which would allow for
+automatic time alignment. However, they usually guarantee to be monotonicly increasing
+and are very accurate when it comes to measuring time differences. Pupil Core software
+is no exception in this regard.
+
+In the example below, we show how one can calculate the offset between a custom "local"
+clock and the clock used in Pupil Core software in real time.
+
 
 ## Local clocks
+When syncing time, one needs to decide on which clock to sync to. This is often
+dependent on the use case and the required measurement precision.
 
-When syncing time, one needs to decide on which clock to sync to. This often is
-dependent on the use case and the required measurement precision. The recommended clock
-for simple Python scripts is `time.perf_counter()` [1]. If you need time measured in
-Unix epoch (less accurate but defines a common starting point), use `time.time()` [2].
-Experiment software like psychopy provide their own clock functions [3].
+For this example, we define the "local" clock as `time.perf_counter()`, which is the
+recommended clock for simple Python scripts [1]. However, you can sample a clock from
+whatever device or software as long as it is monotonic and accurate when measuring time
+differences. For example, experiment software like PsychoPy provide their own clock
+functions [2].
 
-Important: What ever clock function you use, make sure that you use it consistenly
+If you need time measured in Unix epoch (less accurate but defines a common starting
+point), use `time.time()` [3].
+
+Important: Whatever clock function you use, make sure that you use it consistently
 across the experiment. Any change to the clock, e.g. via a reset, invalidates the
-measured clock offset and requires you to remeasure it.
-
-Since Pupil Core measures time in seconds, this script expects the local clock to
-measure time in seconds as well.
-
+measured clock offset and requires you to re-measure it. Since Pupil Core measures time
+in seconds, this script expects the "local" clock to measure time in seconds as well.
 [1] https://docs.python.org/3/library/time.html#time.perf_counter
-[2] https://docs.python.org/3/library/time.html#time.time
-[3] https://www.psychopy.org/api/clock.html
+[2] https://www.psychopy.org/api/clock.html
+[3] https://docs.python.org/3/library/time.html#time.time
+
 
 ## Simplifying assumptions
-
 For this script, we make a few simplifying assumptions. Specifically, we assume that
-both involved clocks (local and remote) do not drift. The term `drift` refers to the
+both involved clocks (local and pupil) do not drift. The term `drift` refers to the
 issue that the clock offsets might change over time due to inaccuracies of the clocks.
 A possible solution is to periodically measure the offset and correct for potential
-drift. In this example, we measure the offset once and assume no drift on the offset.
-
-For a more accurate and stable time sync see
+drift over the course of a given recording. In this example, we measure the offset
+once* and assume no drift on the offset. For a more accurate and stable time sync see
 https://docs.pupil-labs.com/core/software/pupil-capture/#network-plugins
 which is based on https://en.wikipedia.org/wiki/Network_Time_Protocol
 
-## Requirements
+*Below, we show an example of measuring the offset 10 times. This is actually to account
+for variance in the network latency when measuring the offset, rather than clock drift.
+To account for clock drift, one would have to implement periodic offset measurements
+like in the Network_Time_Protocol link above.
 
+## Requirements
 - `time` module; built-in functionality; includes local clock function for this example
 - `pyzmq` module; pip install pyzmq; handles network communication to Core software
 """
@@ -54,23 +63,31 @@ import zmq
 
 
 def main():
-    """This example performs these steps
-    1. Setup network connection (always required)
-    2. Setup local clock function (always required)
-    3. Measure clock offset once
-    4. Measure clock offset more reliably (multiple measurements)
-    5. Infer remote clock time from local clock measurement
     """
+    This example performs these steps:
+    1. Setup network connection (always required)
+    2. Setup "local" clock function (always required)
+    3. Measure clock offset once
+    4. Measure clock offset more reliably to account for network
+    latency variance (multiple measurements)
+    5. Infer remote pupil clock time from local clock measurement
+    """
+
+    # 1. Setup network connection (always required)
     socket = setup_pupil_remote_connection(ip_adress="127.0.0.1")
 
+    # 2. Setup local clock function (always required)
     # local_clock = time.time  # Unix time, less accurate
     # local_clock = psychopy.clock.MonotonicClock().getTime  # new psychopy clock
     # local_clock = existing_psychopy_clock.getTime  # existing psychopy clock
     local_clock = time.perf_counter
 
+    # 3. Measure clock offset once
     offset = measure_clock_offset(socket, clock_function=local_clock)
     print(f"Clock offset (1 measurement): {offset} seconds")
 
+    # 4. Measure clock offset more reliably to account for network
+    # latency variance (multiple measurements)
     number_of_measurements = 10
     stable_offset_mean = measure_clock_offset_stable(
         socket, clock_function=local_clock, nsamples=number_of_measurements
@@ -80,6 +97,7 @@ def main():
         f"{stable_offset_mean} seconds"
     )
 
+    # 5. Infer pupil clock time from "local" clock measurement
     local_time = local_clock()
     pupil_time_calculated_locally = local_time + stable_offset_mean
     print(f"Local time: {local_time}")
@@ -90,7 +108,6 @@ def setup_pupil_remote_connection(
     ip_adress: str = "127.0.0.1", port: int = 50020
 ) -> zmq.Socket:
     """Creates a zmq-REQ socket and connects it to Pupil Capture or Service
-
     See https://docs.pupil-labs.com/developer/core/network-api/ for details.
     """
     ctx = zmq.Context.instance()
@@ -102,7 +119,6 @@ def setup_pupil_remote_connection(
 def request_pupil_time(socket):
     """Uses an existing Pupil Core software connection to request the remote time.
     Returns the current "pupil time" at the timepoint of reception.
-
     See https://docs.pupil-labs.com/core/terminology/#pupil-time for more information
     about "pupil time".
     """
@@ -113,7 +129,6 @@ def request_pupil_time(socket):
 
 def measure_clock_offset(socket, clock_function):
     """Calculates the offset between the Pupil Core software clock and a local clock.
-
     Requesting the remote pupil time takes time. This delay needs to be considered
     when calculating the clock offset. We measure the local time before (A) and
     after (B) the request and assume that the remote pupil time was measured at (A+B)/2,
@@ -121,7 +136,7 @@ def measure_clock_offset(socket, clock_function):
 
     As a result, we have two measurements from two different clocks that were taken
     assumingly at the same point in time. The difference between them ("clock offset")
-    allows us, given a new local clock measurment, to infer the corresponding time on
+    allows us, given a new local clock measurement, to infer the corresponding time on
     the remote clock.
     """
     local_time_before = clock_function()
