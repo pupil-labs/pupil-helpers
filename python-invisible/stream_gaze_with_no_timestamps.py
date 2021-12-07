173d121f32d92221ef8d88e23279c9405f013d6e
Pablo Prietz <pablo@prietz.org>
2021-12-07 17:35:15 +0100

    Pupil Invisible API: Stream gaze without and with relative timestamps

---
+++ python-invisible/stream_gaze_with_no_timestamps.py
@@ -0,0 +1,57 @@
+import asyncio
+import contextlib
+import struct
+
+import requests
+from aiortsp.rtsp.reader import RTSPReader
+
+
+def main():
+    host_name = "pi.local"
+    port = 8080
+    base_url = f"http://{host_name}:{port}/api"
+    api_endpoint_status = "/status"
+    print("Getting stream information...")
+    try:
+        status_response = requests.get(base_url + api_endpoint_status)
+    except requests.exceptions.ConnectionError as err:
+        print(f"Could not connect to server {base_url} ({err})")
+        exit(1)
+    gaze_rtsp_url = get_gaze_rtsp_url(status_response)
+    print(f"Connecting to {gaze_rtsp_url}...")
+
+    with contextlib.suppress(KeyboardInterrupt):
+        asyncio.run(receive_gaze(gaze_rtsp_url))
+
+
+async def receive_gaze(url):
+    async with RTSPReader(url) as reader:
+        async for pkt in reader.iter_packets():
+            # Gaze data contains two big-endian floats for the pixel location and a
+            # single byte indicating whether the user is wearing the device.
+            x, y, worn = struct.unpack("!ffB", pkt.data)
+            print(f"Gaze at [{x:.02f}, {y:.02f}] (worn: {worn == 255})")
+
+
+def get_gaze_rtsp_url(status_response):
+    ip = _get_phone_ip_address(status_response)
+    for sensor in status_response.json()["result"]:
+        if (
+            sensor["model"] == "Sensor"
+            and sensor["data"]["conn_type"] == "DIRECT"
+            and sensor["data"]["sensor"] == "gaze"
+        ):
+            protocol = sensor["data"]["protocol"]
+            port = sensor["data"]["port"]
+            params = sensor["data"]["params"]
+            return f"{protocol}://{ip}:{port}/?{params}"
+
+
+def _get_phone_ip_address(status_response):
+    for sensor in status_response.json()["result"]:
+        if sensor["model"] == "Phone":
+            return sensor["data"]["ip"]
+
+
+if __name__ == "__main__":
+    main()
